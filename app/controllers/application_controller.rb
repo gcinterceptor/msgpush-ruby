ebb6d53a28c9f377311802ffe6896ef3e63e3a8f
Elisio Filho <elisionfilho@gmail.com>
2018-07-27 22:49:33 -0300

    msgpush changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+ @@buffer = [1000]
+ @@index = 0
+
+ def index
+ 	msg = Array(0..1999)
+ 	@@buffer[@@index%@@buffer.size] = msg
+ 	head 200
+ end
+
 end
