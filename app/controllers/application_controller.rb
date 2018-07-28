42924bc0b775039573d0c9acabd34efbd4de195f
Daniel Fireman <danielfireman@gmail.com>
2018-07-28 02:40:08 -0300

    Finally, habemus gci

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::API
- @@buffer = [1000]
+ @@buffer = [1]
  @@index = 0
 
  def index
- 	msg = Array(0..1999)
+	msg = []
+	204800.times{ msg << 'a' } 
  	@@buffer[@@index%@@buffer.size] = msg
  	head 200
  end
