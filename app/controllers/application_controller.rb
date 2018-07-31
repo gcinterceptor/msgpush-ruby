247e89f09d622a58cfdfa23907917f0ce14774b6
Daniel Fireman <danielfireman@gmail.com>
2018-07-31 12:21:03 -0300

    Fixing the controller: creating an actuall with a predefined size and properly looping over its elements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::API
- @@buffer = [1]
+ @@buffer = Array.new(1)
  @@index = 0
 
  def index
 	msg = []
 	204800.times{ msg << 'a' } 
- 	@@buffer[@@index%@@buffer.size] = msg
+	@@buffer[@@index] = msg
+	@@index = (@@index + 1) % @@buffer.size
  	head 200
  end
 
